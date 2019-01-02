08cd3f9c5fe58898a2e1936ed98b5a67bd3c2d5e
Sridhar-S-G <39511658+Sridhar-S-G@users.noreply.github.com>
2019-01-02 22:52:28 +0530

    Create Floyd_triangle.py

---
+++ Floyd_triangle.py
@@ -0,0 +1,14 @@
+'''Print floyd triangle as given below
+input:
+3
+output:
+0
+01
+012 
+'''
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print('')
